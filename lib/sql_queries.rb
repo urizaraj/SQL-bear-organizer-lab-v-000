624d43245ff0f62b7a316e0294aa0423b3349022
Alexander Urizar <urizaraj@gmail.com>
2018-02-22 23:24:49 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  'select color, count(color) as count from bears order by count desc limit 1'
+  'select color, count(color) as count from bears order by count desc'
 end
 
 def counts_number_of_bears_with_goofy_temperaments
